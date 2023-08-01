a275914722e92d094154bc76321d736dda2a01ee
mateusvg <mateusvitorino@outlook.com.br>
2023-08-01 14:15:23 -0300

    annotation for validations on DTO

--- Flights/Dtos/BookDto.cs
+++ Flights/Dtos/BookDto.cs
@@ -1,9 +1,11 @@
-﻿namespace Flights.Dtos
+﻿using System.ComponentModel.DataAnnotations; // Data annotation, são atributos que podemos passar para um campo. Ou seja, FlightId e os outros atributos são necessário para que se ache as informações de voo, passageiro e numero de assentos
+
+namespace Flights.Dtos
 {
     public record BookDto(
-        Guid FlightId,
-        string PassengerEmail,
-        byte NumberOfSeats
+        [Required] Guid FlightId,
+        [Required][EmailAddress][StringLength(100, MinimumLength = 10)] string PassengerEmail,
+        [Required][Range(1, 254)] byte NumberOfSeats
 
         );
 
